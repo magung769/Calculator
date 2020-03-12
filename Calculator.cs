b7712279d0a257b985f534f300b6221cc3ee4cb0
Muhammad Agung Wicaksonosidi <58759829+magung769@users.noreply.github.com>
2020-03-12 17:33:17 +0700

    Calculator.cs
    Menggunkan if else

--- Calculator.cs
+++ Calculator.cs
@@ -1,13 +1,22 @@
-class Calculator
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
 {
-	 static void Main(string[] args)
+    class Calculator
+    {
+        static void Main(string[] args)
         {
-            Console.Title = "Aplikasi Calculator";
-            Console.WriteLine("----------------------------");
+            Console.WriteLine("Aplikasi Calculator");
+			Console.WriteLine();
             Console.WriteLine("1.Penambahan");
             Console.WriteLine("2.Pengurangan");
             Console.WriteLine("3.Perkalian");
             Console.WriteLine("4.Pembagian");
+			Console.WriteLine();
             Console.Write("Masukan Pilihan(1-4):");
             int angka1, angka2, total = 0;
             int pilihan = Int32.Parse(Console.ReadLine());
@@ -55,32 +64,30 @@ class Calculator
 
             Console.WriteLine("\nTekan sembarang key untuk keluar");
 
-
-
-            //Console.WriteLine();
+            // Console.WriteLine();
             // Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
             // Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
             // Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a, b, Perkalian(a, b));
             // Console.WriteLine("Hasil Pengurangan: {0} / {1} = {2}", a, b, Pembagian(a, b));
-
 
             Console.ReadKey();
         }
 
         //static int Penambahan(int a, int b)
         //{
-            //return a + b;
+        //return a + b;
         //}
         //static int Pengurangan(int a, int b)
         //{
-            //return a - b;
+        //return a - b;
         //}
         //static int Perkalian(int a, int b)
         //{
-            //return a * b;
+        //return a * b;
         //}
         //static int Pembagian(int a, int b)
         //{
-            //return a / b;
+        //return a / b;
         //}
     }
+}
