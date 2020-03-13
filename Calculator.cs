5f7fb0a3b16deda6a0c32746634b5314a3e4c76b
magung769 <58759829+magung769@users.noreply.github.com>
2020-03-13 10:35:53 +0700

    Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -11,83 +11,68 @@ namespace ConsoleCalculator
         static void Main(string[] args)
         {
             Console.WriteLine("Aplikasi Calculator");
-			Console.WriteLine();
+            Console.WriteLine();
             Console.WriteLine("1.Penambahan");
             Console.WriteLine("2.Pengurangan");
             Console.WriteLine("3.Perkalian");
             Console.WriteLine("4.Pembagian");
-			Console.WriteLine();
+            Console.WriteLine();
             Console.Write("Masukan Pilihan(1-4):");
-            int angka1, angka2, total = 0;
             int pilihan = Int32.Parse(Console.ReadLine());
 
-            if (pilihan == 1)
+            if (pilihan > 0 && pilihan < 5)
             {
                 Console.Write("Inputkan Nilai A = ");
                 int a = int.Parse(Console.ReadLine());
+
                 Console.Write("Inputkan Nilai B = ");
                 int b = Convert.ToInt32(Console.ReadLine());
-                total = a + b;
-                Console.WriteLine("Jumlah = {0}", total);
-            }
-            else if (pilihan == 2)
-            {
-                Console.Write("Inputkan Nilai A = ");
-                int a = int.Parse(Console.ReadLine());
-                Console.Write("Inputkan Nilai B = ");
-                int b = Convert.ToInt32(Console.ReadLine());
-                total = a - b;
-                Console.WriteLine("Jumlah = {0}", total);
-            }
-            else if (pilihan == 3)
-            {
-                Console.Write("Inputkan Nilai A = ");
-                int a = int.Parse(Console.ReadLine());
-                Console.Write("Inputkan Nilai B = ");
-                int b = Convert.ToInt32(Console.ReadLine());
-                total = a * b;
-                Console.WriteLine("Jumlah = {0}", total);
-            }
-            else if (pilihan == 4)
-            {
-                Console.Write("Inputkan Nilai A = ");
-                int a = int.Parse(Console.ReadLine());
-                Console.Write("Inputkan Nilai B = ");
-                int b = Convert.ToInt32(Console.ReadLine());
-                total = a / b;
-                Console.WriteLine("Jumlah = {0}", total);
+
+                switch (pilihan)
+                {
+                    case 1:
+                    Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+                    break;
+
+                    case 2:
+                    Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+                    break;
+
+                    case 3:
+                    Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a, b, Perkalian(a, b));
+                    break;
+
+                    case 4:
+                    Console.WriteLine("Hasil Pengurangan: {0} / {1} = {2}", a, b, Pembagian(a, b));
+                    break;
+
+                }
             }
             else
             {
-                Console.WriteLine("Tidak ada pilihan");
+                Console.WriteLine("Tidak pilihan tidak tersedia");
             }
 
+            Console.WriteLine();
             Console.WriteLine("\nTekan sembarang key untuk keluar");
-
-            // Console.WriteLine();
-            // Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
-            // Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
-            // Console.WriteLine("Hasil Perkalian: {0} * {1} = {2}", a, b, Perkalian(a, b));
-            // Console.WriteLine("Hasil Pengurangan: {0} / {1} = {2}", a, b, Pembagian(a, b));
-
             Console.ReadKey();
         }
 
-        //static int Penambahan(int a, int b)
-        //{
-        //return a + b;
-        //}
-        //static int Pengurangan(int a, int b)
-        //{
-        //return a - b;
-        //}
-        //static int Perkalian(int a, int b)
-        //{
-        //return a * b;
-        //}
-        //static int Pembagian(int a, int b)
-        //{
-        //return a / b;
-        //}
+        static int Penambahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int Pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int Perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int Pembagian(int a, int b)
+        {
+            return a / b;
+        }
     }
 }
